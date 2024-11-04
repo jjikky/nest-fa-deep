dfd64e282f6a82d210456b5c664c7642ec2a207f
jjikky <kok5417@skuniv.ac.kr>
2024-11-04 15:00:03 +0900

    feat: Swagger Bearer 인증 설정
    - Swagger Bearer 인증 옵션 추가
    - API 엔드포인트에 Bearer 인증 적용
    - persistAuthorization 옵션 설정

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
-import { ApiExtraModels, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiExtraModels, ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
 import { FindUserReqDto } from './dto/req.dto';
 import { PageReqDto } from 'src/common/dto/req.dto';
 import { ApiGetItemsResponse, ApiGetResponse } from 'src/common/decorator/swagger.decorator';
 import { FindUserResDto } from './dto/res.dto';
+import { User } from 'src/common/decorator/user.decorator';
+import { UserAfterAuth } from 'src/common/decorator/user.decorator';
 
 @ApiTags('User')
 @ApiExtraModels(FindUserReqDto, PageReqDto)
@@ -12,12 +14,14 @@ import { FindUserResDto } from './dto/res.dto';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiBearerAuth()
   @ApiGetItemsResponse(FindUserResDto)
   @Get()
-  findAll(@Query() { page, size }: PageReqDto) {
+  findAll(@Query() { page, size }: PageReqDto, @User() user: UserAfterAuth) {
     return this.userService.findAll();
   }
 
+  @ApiBearerAuth()
   @ApiGetResponse(FindUserResDto)
   @Get(':id')
   findOne(@Param() { id }: FindUserReqDto) {
