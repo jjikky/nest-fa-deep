1daf43e56d714aa8f86c2e7bac5f7f119bfe57bd
jjikky <kok5417@skuniv.ac.kr>
2024-11-04 14:59:51 +0900

    feat: 인증 컨트롤러 및 Public 라우트 구현
    - 회원가입/로그인 엔드포인트 구현
    - Public 데코레이터 적용
    - 요청 파라미터 타입 개선

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, BadRequestException } from '@nestjs/common';
 import { ApiExtraModels, ApiTags } from '@nestjs/swagger';
 import { SigninReqDto, SignupReqDto } from './dto/req.dto';
 import { SignInResDto } from './dto/res.dto';
 import { SignUpResDto } from './dto/res.dto';
 import { AuthService } from './auth.service';
 import { ApiPostResponse } from 'src/common/decorator/swagger.decorator';
+import { Public } from 'src/common/decorator/public.decorator';
 
 @ApiTags('Auth')
 @ApiExtraModels(SignUpResDto, SignInResDto)
@@ -13,14 +14,18 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @ApiPostResponse(SignUpResDto)
+  @Public()
   @Post('signup')
-  async signup(@Body() signupReqDto: SignupReqDto) {
-    return this.authService.signup('email', 'password');
+  async signup(@Body() { email, password, passwordConfirm }: SignupReqDto) {
+    if (password !== passwordConfirm) throw new BadRequestException();
+    const { id } = await this.authService.signup(email, password);
+    return { id };
   }
 
   @ApiPostResponse(SignInResDto)
+  @Public()
   @Post('signin')
-  async signin(@Body() signinReqDto: SigninReqDto) {
-    return this.authService.signin(signinReqDto);
+  async signin(@Body() { email, password }: SigninReqDto) {
+    return this.authService.signin(email, password);
   }
 }
