70e5002461b4e12c3c4f965aedf3b76e10f5e918
jjikky <kok5417@skuniv.ac.kr>
2024-11-08 20:30:23 +0900

    feat: ThrottlerModule 글로벌 설정 추가
    3가지 throttling 정책 추가
    - short: 1초당 3회
    - medium: 10초당 20회
    - long: 1분당 100회

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,27 @@ import postgresConfig from './config/postgres.config';
 import jwtConfig from './config/jwt.config';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import swaggerConfig from './config/swagger.config';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 20,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
     ConfigModule.forRoot({ isGlobal: true, load: [postgresConfig, jwtConfig, swaggerConfig] }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
